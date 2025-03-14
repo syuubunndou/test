655f7baf1a5eb54ab8a79976dd73c82a782bd5f6
syuubunndou <zin.syuubunndou@gmail.com>
2025-03-14 12:55:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
+import hashlib
+from cryptography.fernet import Fernet
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # CORSを許可
+CORS(app, resources={r"/encrypt": {"origins": "*"}})  # 必要に応じてオリジンを指定
 
 @app.route('/get_message', methods=['GET'])
 def get_message():
     return jsonify({"message": "foo"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
